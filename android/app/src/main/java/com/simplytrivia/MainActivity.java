37fe9d93bf808ff03c6b1e9013aa94fcfcddd44b
Jarod Legault <jlegault@cntr.al>
2021-09-29 20:00:41 -0600

    Install and configure React Navigation 6.

--- android/app/src/main/java/com/simplytrivia/MainActivity.java
+++ android/app/src/main/java/com/simplytrivia/MainActivity.java
@@ -1,5 +1,7 @@
 package com.simplytrivia;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +13,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SimplyTrivia";
+  }
+
+  /**
+   * Required by React Navigation / react-native-screens.
+   * @param savedInstanceState
+   */
+  @Override
+  protected void onCreate( Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
