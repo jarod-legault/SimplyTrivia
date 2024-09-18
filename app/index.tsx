724719235e0e9188f57629f7b2a02abcf9226096
Jarod Legault <jarod.legault@gmail.com>
2024-09-17 20:40:59 -0600

    Handle network error on home screen.

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { Stack, Link } from 'expo-router';
-import { useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import { Container } from '~/components/Container';
 import DifficultyButton from '~/components/DifficultyButton';
@@ -9,27 +17,36 @@ import DifficultyButton from '~/components/DifficultyButton';
 export default function Home() {
   const [OTDBToken, setOTDBToken] = useState<string | null>(null);
   const [isFetchingToken, setIsFetchingToken] = useState<boolean>(false);
+  const [networkError, setNetworkError] = useState<string>('');
 
-  async function getOTDBToken() {
-    setIsFetchingToken(true);
+  useEffect(() => {
+    let attemptCount = 0;
+    async function getOTDBToken() {
+      setIsFetchingToken(true);
 
-    try {
-      const response = await axios.get('https://opentdb.com/api_token.php', {
-        params: {
-          command: 'request',
-        },
-      });
-      setOTDBToken(response.data.token);
-    } catch (error) {
-      console.error(error); // TODO: Handle errors better.
-    } finally {
-      setIsFetchingToken(false);
+      try {
+        attemptCount++;
+        const response = await axios.get('https://opentdb.com/api_token.php', {
+          params: {
+            command: 'request',
+          },
+        });
+        setOTDBToken(response.data.token);
+        setIsFetchingToken(false);
+      } catch (error) {
+        if (attemptCount < 10) {
+          await getOTDBToken();
+        } else {
+          setNetworkError((error as Error).message);
+          setIsFetchingToken(false);
+          console.error(error);
+        }
+      }
     }
-  }
 
-  if (!isFetchingToken && !OTDBToken) {
     getOTDBToken();
-  }
+  }, []);
+
   return (
     <>
       <Stack.Screen options={{ title: 'home', headerShown: false }} />
@@ -39,17 +56,42 @@ export default function Home() {
             <Image style={styles.logo} source={require('../assets/logo.png')} />
             <Text style={styles.headerText}>Simply Trivia</Text>
           </View>
-          <Link href={{ pathname: '/question', params: { difficulty: 'easy', OTDBToken } }} asChild>
-            <DifficultyButton difficulty="easy" />
-          </Link>
-          <Link
-            href={{ pathname: '/question', params: { difficulty: 'medium', OTDBToken } }}
-            asChild>
-            <DifficultyButton difficulty="medium" />
-          </Link>
-          <Link href={{ pathname: '/question', params: { difficulty: 'hard', OTDBToken } }} asChild>
-            <DifficultyButton difficulty="hard" />
-          </Link>
+
+          <View style={styles.body}>
+            {isFetchingToken && (
+              <View style={{ flex: 1, justifyContent: 'center' }}>
+                <ActivityIndicator size="large" />
+              </View>
+            )}
+
+            {networkError && (
+              <Link replace href={{ pathname: '/', params: {} }} asChild>
+                <TouchableOpacity style={styles.retryButton}>
+                  <Text style={styles.retryButtonText}>Network Error - Retry</Text>
+                </TouchableOpacity>
+              </Link>
+            )}
+
+            {OTDBToken && (
+              <>
+                <Link
+                  href={{ pathname: '/question', params: { difficulty: 'easy', OTDBToken } }}
+                  asChild>
+                  <DifficultyButton difficulty="easy" />
+                </Link>
+                <Link
+                  href={{ pathname: '/question', params: { difficulty: 'medium', OTDBToken } }}
+                  asChild>
+                  <DifficultyButton difficulty="medium" />
+                </Link>
+                <Link
+                  href={{ pathname: '/question', params: { difficulty: 'hard', OTDBToken } }}
+                  asChild>
+                  <DifficultyButton difficulty="hard" />
+                </Link>
+              </>
+            )}
+          </View>
         </ScrollView>
       </Container>
     </>
@@ -60,7 +102,13 @@ const styles = StyleSheet.create({
   headerStyle: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 50,
+    marginTop: 100,
+  },
+  body: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
     height: 100,
@@ -74,13 +122,22 @@ const styles = StyleSheet.create({
   difficultyContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   easyBorder: {
     borderColor: '#009D40',
   },
   mediumBorder: {
     borderColor: '#0e0fe0',
+  },
+  retryButton: {
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 10,
+    backgroundColor: '#0e0fe0',
+  },
+  retryButtonText: {
+    color: 'white',
+    fontSize: 20,
   },
   hardBorder: {
     borderColor: '#FF570D',
